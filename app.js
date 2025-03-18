c5402c5bdc2f5a3bcad80f5deccf266746019ef2
Leandrof5 <leandrofabio2323@gmail.com>
2025-03-17 22:41:29 -0300

    Adicionando app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value.trim();
+
+    if (nomeAmigo === "") {
+        alert("nenhum nome disponivel para adicionar.");
+        return;
+    }
+    amigos.push(nomeAmigo);
+
+    document.getElementById("amigo").value = "";
+
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+
+    amigos.forEach(function(amigo) {
+        let itemLista = document.createElement("li");
+        itemLista.textContent = amigo;
+        listaAmigos.appendChild(itemLista);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("nao ha amigos para sortear.");
+        return;
+    }
+}
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos [indiceSorteado];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+
+    let itemresultado = document.getElementById("li");
+    itemresultado.textContent = " Amigo Sorteado: " + amigoSorteado + "";
+    resultado.appendChild(itemresultado);
