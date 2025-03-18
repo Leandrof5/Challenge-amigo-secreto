f1e5eaf0a24322ed1c8971017ccd9a23ea96663d
Leandrof5 <leandrofabio2323@gmail.com>
2025-03-17 23:02:50 -0300

    tentando corrigir bugs

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-let amigos = [];
-
+let amigos = []; 
 function adicionarAmigo() {
-    let nomeAmigo = document.querySelector('input').value.trim();
+    let nomeAmigo = document.getElementById("amigo").value.trim();
 
     if (nomeAmigo === "") {
-        alert("nenhum nome disponivel para adicionar.");
+        alert("Por favor, digite um nome antes de adicionar.");
         return;
     }
+
     amigos.push(nomeAmigo);
 
     document.getElementById("amigo").value = "";
 
     atualizarListaAmigos();
 }
-
 function atualizarListaAmigos() {
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = ""; 
 
     amigos.forEach(function(amigo) {
-        let itemLista = document.createElement("li");
+        let itemLista = document.createElement("li"); 
         itemLista.textContent = amigo;
         listaAmigos.appendChild(itemLista);
     });
 }
-
 function sortearAmigo() {
     if (amigos.length === 0) {
-        alert("nao ha amigos para sortear.");
+        alert("Não há amigos disponíveis para sortear.");
         return;
     }
-}
+
     let indiceSorteado = Math.floor(Math.random() * amigos.length);
-    let amigoSorteado = amigos [indiceSorteado];
+    let amigoSorteado = amigos[indiceSorteado];
 
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = "";
 
-    let itemresultado = document.getElementById("li");
-    itemresultado.textContent = " Amigo Sorteado: " + amigoSorteado + "";
-    resultado.appendChild(itemresultado);
+    let itemResultado = document.createElement("li");
+    itemResultado.textContent = " Amigo Sorteado: " + amigoSorteado ;
+    resultado.appendChild(itemResultado);
+}
